ab8dec9dd6826591e215c07ed2a9d3bdb661ea57
Founder <55168055+Founderroni@users.noreply.github.com>
2023-03-19 15:47:18 -0400

    panel & label here for now

--- KaffeeUtility/KaffeeUtility/Tabs/Injector.Designer.cs
+++ KaffeeUtility/KaffeeUtility/Tabs/Injector.Designer.cs
@@ -28,20 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ClientPanel = new Guna.UI2.WinForms.Guna2Panel();
+            this.ClientLabel = new System.Windows.Forms.Label();
+            this.ClientPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // ClientPanel
+            // 
+            this.ClientPanel.BackColor = System.Drawing.Color.Transparent;
+            this.ClientPanel.Controls.Add(this.ClientLabel);
+            this.ClientPanel.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientPanel.Location = new System.Drawing.Point(295, 79);
+            this.ClientPanel.Name = "ClientPanel";
+            this.ClientPanel.ShadowDecoration.Depth = 0;
+            this.ClientPanel.Size = new System.Drawing.Size(190, 151);
+            this.ClientPanel.TabIndex = 7;
+            this.ClientPanel.UseTransparentBackground = true;
+            // 
+            // ClientLabel
+            // 
+            this.ClientLabel.AutoSize = true;
+            this.ClientLabel.ForeColor = System.Drawing.Color.Gray;
+            this.ClientLabel.Location = new System.Drawing.Point(72, 0);
+            this.ClientLabel.Name = "ClientLabel";
+            this.ClientLabel.Size = new System.Drawing.Size(46, 15);
+            this.ClientLabel.TabIndex = 2;
+            this.ClientLabel.Text = "Clients";
             // 
             // Injector
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.Controls.Add(this.ClientPanel);
             this.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.LightGray;
             this.Name = "Injector";
             this.Size = new System.Drawing.Size(811, 548);
+            this.ClientPanel.ResumeLayout(false);
+            this.ClientPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel ClientPanel;
+        private System.Windows.Forms.Label ClientLabel;
     }
 }
