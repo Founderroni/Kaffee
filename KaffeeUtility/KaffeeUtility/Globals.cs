5d1cb6b06642e60a5c49828b7c0179826221e178
Founder <55168055+Founderroni@users.noreply.github.com>
2023-03-21 20:39:59 -0400

    remove useless line & use x64

--- KaffeeUtility/KaffeeUtility/Globals.cs
+++ KaffeeUtility/KaffeeUtility/Globals.cs
@@ -7,7 +7,7 @@ namespace KaffeeUtility
 {
     public class Globals
     {
-        public static readonly float Version = 1.0F;
+        public static readonly float Version = 0.1F;
         public readonly static string McpeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftpe\";
         public static readonly string RootDataDir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\FadedSolutions";
         public static readonly string DataDir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\FadedSolutions\Kaffee";
@@ -17,9 +17,5 @@ namespace KaffeeUtility
 
         public static List<ClientListStruct> ClientList = new List<ClientListStruct>();
         public static List<SpoofPointersStruct> SpoofList = new List<SpoofPointersStruct>();
-
-        /*
-         Icon Color: #232323
-         */
     }
 }
