ee610b474858d4f9fd786b264e1ddc451f38ba12
Founder <55168055+Founderroni@users.noreply.github.com>
2023-05-27 14:38:17 -0400

    Catch IOException error from GetConfig()

--- KaffeeUtility/KaffeeUtility/Utils/Config.cs
+++ KaffeeUtility/KaffeeUtility/Utils/Config.cs
@@ -1,10 +1,20 @@
 ﻿using Config.Net;
 using KaffeeUtility.Models;
+using System.IO;
 
 namespace KaffeeUtility.Utils
 {
     internal static class Config
     {
-        public static ConfigData GetConfig() => new ConfigurationBuilder<ConfigData>().UseJsonFile(Globals.ConfigFile).Build();
+        public static ConfigData GetConfig()
+        {
+            try
+            {
+                return new ConfigurationBuilder<ConfigData>().UseJsonFile(Globals.ConfigFile).Build();
+            } catch (IOException)
+            {
+                return null;
+            }
+        }
     }
 }
