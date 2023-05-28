269b673ec30818a85eb4171c1972fc445940175c
Founder <55168055+Founderroni@users.noreply.github.com>
2023-05-27 20:44:00 -0400

    Add the rest

--- KaffeeUtility/KaffeeUtility/Utils/Config.cs
+++ KaffeeUtility/KaffeeUtility/Utils/Config.cs
@@ -6,8 +6,11 @@ namespace KaffeeUtility.Utils
 {
     internal static class Config
     {
-        public static readonly string AppConfigFile = Path.Combine(Globals.DataDir, "AppConfig.json");
-        public static readonly string StatsFile = Path.Combine(Globals.DataDir, "Stats.json");
+        public static readonly string AppConfigFile = Path.Combine(Globals.DataDir, "ApplicationConfig.json");
+        public static readonly string StatsFile = Path.Combine(Globals.DataDir, "Statistics.json");
+        public static readonly string InjectConfigFile = Path.Combine(Globals.DataDir, "InjectionConfig.json");
+        public static readonly string SpooferConfigFile = Path.Combine(Globals.DataDir, "SpooferConfig.json");
+        public static readonly string DiscordConfigFile = Path.Combine(Globals.DataDir, "DiscordConfig.json");
 
         public static ConfigData GetConfig()
         {
@@ -43,6 +46,45 @@ namespace KaffeeUtility.Utils
             catch (IOException)
             {
                 Logging.Log("IOException occured in GetStats");
+                return null;
+            }
+        }
+
+        public static InjectConfig GetInjectConfig()
+        {
+            try
+            {
+                return new ConfigurationBuilder<InjectConfig>().UseJsonFile(InjectConfigFile).Build();
+            }
+            catch (IOException)
+            {
+                Logging.Log("IOException occured in GetInjectConfig");
+                return null;
+            }
+        }
+
+        public static SpooferConfig GetSpooferConfig()
+        {
+            try
+            {
+                return new ConfigurationBuilder<SpooferConfig>().UseJsonFile(SpooferConfigFile).Build();
+            }
+            catch (IOException)
+            {
+                Logging.Log("IOException occured in GetSpooferConfig");
+                return null;
+            }
+        }
+
+        public static DiscordConfig GetDiscordConfig()
+        {
+            try
+            {
+                return new ConfigurationBuilder<DiscordConfig>().UseJsonFile(DiscordConfigFile).Build();
+            }
+            catch (IOException)
+            {
+                Logging.Log("IOException occured in GetDiscordConfig");
                 return null;
             }
         }
