84d172fe19f8b1f12bd6f7c7071b6ae6ace9960e
Founder <55168055+Founderroni@users.noreply.github.com>
2023-05-27 20:36:30 -0400

    Adding the Get functions

--- KaffeeUtility/KaffeeUtility/Globals.cs
+++ KaffeeUtility/KaffeeUtility/Globals.cs
@@ -11,13 +11,13 @@ namespace KaffeeUtility
         public static readonly float Version = 0.5F;
         public static string LatestVersion = "0"; // This should change
         public readonly static string McpeDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftpe\");
+        public static string OptionsFile = Path.Combine(McpeDirectory, "options.txt");
         public static string RootDataDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "FadedSolutions");
         public static readonly string DataDir = Path.Combine(RootDataDir, "Kaffee");
         public static readonly string AppDir = AppDomain.CurrentDomain.BaseDirectory;
         public static readonly string AppPath = Assembly.GetExecutingAssembly().Location;
         public static readonly string LogFile = Path.Combine(DataDir, "Log.txt");
         public static readonly string ConfigFile = Path.Combine(DataDir, "Config.json");
-        public static string OptionsFile = Path.Combine(McpeDirectory, "options.txt");
 
         public static List<ClientListStruct> ClientList = new List<ClientListStruct>();
         public static List<SpoofPointersStruct> SpoofList = new List<SpoofPointersStruct>();
--- KaffeeUtility/KaffeeUtility/Utils/Config.cs
+++ KaffeeUtility/KaffeeUtility/Utils/Config.cs
@@ -6,6 +6,9 @@ namespace KaffeeUtility.Utils
 {
     internal static class Config
     {
+        public static readonly string AppConfigFile = Path.Combine(Globals.DataDir, "AppConfig.json");
+        public static readonly string StatsFile = Path.Combine(Globals.DataDir, "Stats.json");
+
         public static ConfigData GetConfig()
         {
             try
@@ -14,6 +17,32 @@ namespace KaffeeUtility.Utils
             } catch (IOException)
             {
                 Logging.Log("IOException occured in GetConfig");
+                return null;
+            }
+        }
+
+        public static AppConfig GetAppConfig()
+        {
+            try
+            {
+                return new ConfigurationBuilder<AppConfig>().UseJsonFile(AppConfigFile).Build();
+            }
+            catch (IOException)
+            {
+                Logging.Log("IOException occured in GetAppConfig");
+                return null;
+            }
+        }
+
+        public static Stats GetStats()
+        {
+            try
+            {
+                return new ConfigurationBuilder<Stats>().UseJsonFile(StatsFile).Build();
+            }
+            catch (IOException)
+            {
+                Logging.Log("IOException occured in GetStats");
                 return null;
             }
         }
