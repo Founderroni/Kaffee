d20b05d1864db1deee3a20126feeca57eee9f4e5
Founder <55168055+Founderroni@users.noreply.github.com>
2023-07-01 22:10:04 -0400

    Return CachedConfig in GetConfig

--- KaffeeUtility/KaffeeUtility/Utils/Config.cs
+++ KaffeeUtility/KaffeeUtility/Utils/Config.cs
@@ -16,7 +16,10 @@ namespace KaffeeUtility.Utils
         {
             try
             {
-                return new ConfigurationBuilder<ConfigData>().UseJsonFile(Globals.ConfigFile).Build();
+                if (Globals.CachedConfig == null)
+                    return new ConfigurationBuilder<ConfigData>().UseJsonFile(Globals.ConfigFile).Build();
+                else
+                    return Globals.CachedConfig;
             } catch (IOException)
             {
                 Logging.Log("IOException occured in GetConfig");
@@ -27,71 +30,6 @@ namespace KaffeeUtility.Utils
         public static void ResetConfigs()
         {
 
-        }
-
-        public static AppConfig GetAppConfig()
-        {
-            try
-            {
-                return new ConfigurationBuilder<AppConfig>().UseJsonFile(AppConfigFile).Build();
-            }
-            catch (IOException)
-            {
-                Logging.Log("IOException occured in GetAppConfig");
-                return null;
-            }
-        }
-
-        public static Stats GetStats()
-        {
-            try
-            {
-                return new ConfigurationBuilder<Stats>().UseJsonFile(StatsFile).Build();
-            }
-            catch (IOException)
-            {
-                Logging.Log("IOException occured in GetStats");
-                return null;
-            }
-        }
-
-        public static InjectConfig GetInjectConfig()
-        {
-            try
-            {
-                return new ConfigurationBuilder<InjectConfig>().UseJsonFile(InjectConfigFile).Build();
-            }
-            catch (IOException)
-            {
-                Logging.Log("IOException occured in GetInjectConfig");
-                return null;
-            }
-        }
-
-        public static SpooferConfig GetSpooferConfig()
-        {
-            try
-            {
-                return new ConfigurationBuilder<SpooferConfig>().UseJsonFile(SpooferConfigFile).Build();
-            }
-            catch (IOException)
-            {
-                Logging.Log("IOException occured in GetSpooferConfig");
-                return null;
-            }
-        }
-
-        public static DiscordConfig GetDiscordConfig()
-        {
-            try
-            {
-                return new ConfigurationBuilder<DiscordConfig>().UseJsonFile(DiscordConfigFile).Build();
-            }
-            catch (IOException)
-            {
-                Logging.Log("IOException occured in GetDiscordConfig");
-                return null;
-            }
         }
     }
 }
