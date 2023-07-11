35fecf6f552313fd6bc145cdf73707975041bfd0
Founder <55168055+Founderroni@users.noreply.github.com>
2023-07-11 12:28:26 -0400

    Set the correct one & delete old interfaces

--- KaffeeUtility/KaffeeUtility/Models/ConfigData.cs
+++ KaffeeUtility/KaffeeUtility/Models/ConfigData.cs
@@ -23,7 +23,7 @@ namespace KaffeeUtility.Models
         [Option(DefaultValue = false)]
         bool GithubBlocked { get; set; }
 
-        [Option(DefaultValue = false)]
+        [Option(DefaultValue = true)]
         bool DownloadInParallel { get; set; }
 
         [Option(DefaultValue = true)]
@@ -94,98 +94,5 @@ namespace KaffeeUtility.Models
         [Option(DefaultValue = true)]
         bool RpcEnabled { get; set; }
         #endregion
-    }
-
-    public interface AppConfig
-    {
-        [Option(DefaultValue = "null")]
-        string RootDirectory { get; set; }
-
-        [Option(DefaultValue = true)]
-        bool Logging { get; set; }
-
-        [Option(DefaultValue = false)]
-        bool FastLaunch { get; set; }
-
-        [Option(DefaultValue = false)]
-        bool SkipUpdateCheck { get; set; }
-
-        [Option(DefaultValue = true)]
-        bool UseAnimations { get; set; }
-
-        [Option(DefaultValue = false)]
-        bool GithubBlocked { get; set; }
-
-        [Option(DefaultValue = true)]
-        bool DownloadInParallel { get; set; }
-    }
-
-    public interface Stats
-    {
-        [Option(DefaultValue = 0)]
-        int Launches { get; set; }
-
-        [Option(DefaultValue = 0)]
-        int Injections { get; set; }
-
-        [Option(DefaultValue = 0)]
-        int FailedInjections { get; set; }
-
-        [Option(DefaultValue = 0)]
-        int CidSpoofs { get; set; }
-
-        [Option(DefaultValue = 0)]
-        int DidSpoofs { get; set; }
-
-        [Option(DefaultValue = 0)]
-        int McidSpoofs { get; set; }
-    }
-
-    public interface InjectConfig
-    {
-        [Option(DefaultValue = 0)]
-        int InjectDelay { get; set; }
-
-        [Option(DefaultValue = 10)]
-        int LaunchDelay { get; set; }
-
-        [Option(DefaultValue = 0)]
-        int ClientIndex { get; set; }
-
-        [Option(DefaultValue = "")]
-        string CustomDllPath { get; set; }
-
-        [Option(DefaultValue = false)]
-        bool AutoOpenMc { get; set; }
-
-        [Option(DefaultValue = false)]
-        bool DisableVersionCheck { get; set; }
-
-        [Option(DefaultValue = false)]
-        bool UseCustomDll { get; set; }
-
-        [Option(DefaultValue = true)]
-        bool KillAllInstances { get; set; }
-
-        [Option(DefaultValue = true)]
-        bool InjectToAll { get; set; }
-    }
-
-    public interface SpooferConfig
-    {
-        [Option(DefaultValue = "")]
-        string CustomDid { get; set; }
-    }
-
-    public interface DiscordConfig
-    {
-        [Option(DefaultValue = "")]
-        string RpcDetail { get; set; }
-
-        [Option(DefaultValue = "")]
-        string RpcState { get; set; }
-
-        [Option(DefaultValue = true)]
-        bool RpcEnabled { get; set; }
     }
 }
